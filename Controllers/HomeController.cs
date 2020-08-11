89767c76d6790af3dd6dc19f280abfe98667e68f
Adrien Dion <adion81@gmail.com>
2020-08-11 11:49:58 -0500

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyCoolSite.Models;
+
+namespace MyCoolSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string welcome = "Welcome to My Cool Site";
+            return View("Index",welcome);
+        }
+
+        [HttpPost("process")]
+        public IActionResult Process(Sandwich newSand)
+        {
+            Console.WriteLine(newSand.Name);
+            return View("Results",newSand);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Sandwich.cs
@@ -0,0 +1,11 @@
+namespace MyCoolSite.Models
+{
+    public class Sandwich
+    {
+        public string Name {get;set;}
+        public string ProteinOne {get;set;}
+        public string ProteinTwo {get;set;}
+        public string Bread {get;set;}
+        public string Instructions {get;set;}
+    }
+}
